fa29a3e9db51fce768153d691f1719570d7c4af8
Lisa Ngo <lisango.e@gmail.com>
2019-10-09 23:33:37 +0200

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'save_the_dates#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :rooms
   resources :save_the_dates
