df8e12a700238de4e4bca8cf27a27b83cd2b064e
Lisa Ngo <lisango.e@gmail.com>
2019-10-12 18:43:13 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 SaveTheDate.destroy_all
+Save.destroy_all
 Room.destroy_all
 
 
